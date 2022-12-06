996c2b24cc759e4cd2d020803b15b51b26adf66b
Simon Schmitt <mail@simonschmitt.com>
2022-12-06 17:59:47 +0100

    test: Add integration tests for existing superagent support

---
+++ integration_tests/superagent.int.test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const superagent = require('superagent');
+
+describe('superagent', () => {
+  let server;
+  let url;
+
+  const body = { name: 'john' };
+  const headerName = 'Some';
+  const headerValue = 'Header';
+
+  let response;
+
+  // eslint-disable-next-line jest/no-done-callback
+  beforeAll((done) => {
+    // eslint-disable-next-line new-cap
+    const app = new express();
+    app.get('/', (req, res) => {
+      res
+        .status(200)
+        .set(headerName, headerValue)
+        .json(body);
+    });
+
+    server = http.createServer(app);
+    const listener = server.listen(async () => {
+      url = `http://localhost:${listener.address().port}`;
+      response = await superagent.get(`${url}/`);
+      server.close(done);
+    });
+  });
+
+  describe('toReturnHttpCode', () => {
+    it('should succeed if http code is as expected', async () => {
+      expect(response).toReturnHttpCode(200);
+    });
+
+    it('should fail if http code is not as expected', async () => {
+      let caughtError;
+      try {
+        expect(response).toReturnHttpCode(500);
+      } catch (error) {
+        caughtError = error;
+      }
+
+      expect(caughtError.message).toContain('expected http status code 200 to equal 500\n');
+      expect(caughtError.message).toContain(`"${headerName.toLowerCase()}": "${headerValue}"`);
+    });
+  });
+
+  describe('toReturnHttpHeader', () => {
+    it('should succeed if http header is as expected', async () => {
+      expect(response).toReturnHttpHeader(headerName, headerValue);
+    });
+
+    it('should fail if http header is not as expected', async () => {
+      let caughtError;
+      try {
+        expect(response).toReturnHttpHeader(headerName, '');
+      } catch (error) {
+        caughtError = error;
+      }
+
+      expect(caughtError.message).toContain(
+        `expected http header "${headerName.toLowerCase()}" with value ""\n`
+              + '\n'
+              + 'server responded with body:\n'
+              + `${JSON.stringify(body, null, 2)}\n`
+              + '\n'
+              + 'server responded with headers:\n',
+      );
+      expect(caughtError.message).toContain(`"${headerName.toLowerCase()}": "${headerValue}"`);
+    });
+  });
+});
